de403c293314c8abf3225cbaaafa4d0d75cc87ac
milkavladislav <milkav06062003@gmail.com>
2023-03-05 09:11:06 +0200

    add all properties displaying

--- src/components/common/PropertyCard.tsx
+++ src/components/common/PropertyCard.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import { Place } from "@mui/icons-material";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Stack,
+  Typography,
+} from "@pankod/refine-mui";
+import { PropertyCardProps } from "interfaces/property";
+import { Link } from "@pankod/refine-react-router-v6";
 
-const PropertyCard = () => {
+const PropertyCard = ({
+  id,
+  title,
+  location,
+  photo,
+  price,
+}: PropertyCardProps) => {
   return (
-    <div>PropertyCard</div>
-  )
-}
+    <Card
+      component={Link}
+      to={`/properties/${id}`}
+      sx={{
+        maxWidth: "330px",
+        padding: "10px",
+        "&:hover": { boxShadow: "0 22px 45px 2px rgba(176, 176, 176, 0.1" },
+        cursor: "pointer",
+      }}
+      elevation={0}
+    >
+      <CardMedia
+        component="img"
+        width="100%"
+        height={210}
+        image={photo}
+        alt="card image"
+        sx={{ borderRadius: "10px" }}
+      />
+      <CardContent
+        sx={{
+          paddingX: "5px",
+          display: "flex",
+          flexDirection: "row",
+          gap: "10px",
+          justifyContent: "space-between",
+          textDecoration: "none",
+        }}
+      >
+        <Stack direction="column" gap={1}>
+          <Typography fontSize={16} fontWeight={500} color="#11142d">{title}</Typography>
+          <Stack direction="row" alignItems="flex-start" gap={0.5}>
+            <Place sx={{ fontSize: 18, color: "#11142d", marginTop: 0.5 }} />
+            <Typography fontSize={14} color="#808191">{location}</Typography>
+          </Stack>
+        </Stack>
+        <Box px={1.5} py={0.5} borderRadius={1} bgcolor="#dadefa" height="fit-content">
+          <Typography fontSize={12} fontWeight={600} color="#475be8">
+            ${price}
+          </Typography>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default PropertyCard
+export default PropertyCard;
--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,11 +1,23 @@
 import { Add } from "@mui/icons-material";
+import { useTable } from "@pankod/refine-core";
 import { Box, Stack, Typography } from "@pankod/refine-mui";
 import { useNavigate } from "@pankod/refine-react-router-v6";
-import { CustomButton } from "components";
+import { CustomButton, PropertyCard } from "components";
 import React from "react";
 
 const AllProperties = () => {
   const navigate = useNavigate();
+
+  const {
+    tableQueryResult: { data, isLoading, isError },
+  } = useTable();
+
+  console.log(data);
+
+  const allProperties = data?.data ?? [];
+
+  if (isLoading) return <Typography>Loading...</Typography>;
+  if (isError) return <Typography>Error...</Typography>;
 
   return (
     <Box>
@@ -21,6 +33,25 @@ const AllProperties = () => {
           icon={<Add />}
         ></CustomButton>
       </Stack>
+      <Box
+        mt="20px"
+        sx={{
+          display: "flex",
+          gap: 3,
+          flexWrap: "wrap",
+        }}
+      >
+        {allProperties.map((property) => (
+          <PropertyCard
+            id={property._id}
+            title={property.title}
+            location={property.location}
+            price={property.price}
+            photo={property.photo}
+            key={property._id}
+          />
+        ))}
+      </Box>
     </Box>
   );
 };
